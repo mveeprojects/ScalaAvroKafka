cd2261067d48cc9846919da78a58c16b407b7f84
mva13 <markvallis1987@Gmail.com>
2021-02-10 00:27:23 +0000

    Simplified Kafka Avro producer

--- src/main/scala/AvroProducer.scala
+++ src/main/scala/AvroProducer.scala
@@ -1,28 +1,20 @@
 import com.sksamuel.avro4s.{AvroSchema, ToRecord}
 import conf.Config
-import io.confluent.kafka.schemaregistry.client.{CachedSchemaRegistryClient, SchemaRegistryClient}
-import io.confluent.kafka.serializers.AbstractKafkaAvroSerDeConfig._
-import io.confluent.kafka.serializers.KafkaAvroSerializer
-import io.confluent.kafka.serializers.subject.TopicNameStrategy
 import org.apache.avro.Schema
 import org.apache.avro.generic._
 import org.apache.kafka.clients.producer._
-import org.apache.kafka.common.serialization.{Serializer => KafkaSerializer}
 import utils.Logging
 
-import java.util
 import java.util.Properties
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
 import scala.concurrent.{Await, Future}
-import scala.jdk.CollectionConverters._
 import scala.util.{Failure, Success}
 
 object AvroProducer extends App with Config with Logging {
 
   val schema: Schema                                 = AvroSchema[Person]
   val props: Properties                              = setupKafkaProps
-  val kafkaAvroSerializer: KafkaAvroSerializer       = configureAvroSerializer
   val producer: KafkaProducer[String, GenericRecord] = new KafkaProducer[String, GenericRecord](props)
 
   List(
@@ -34,8 +26,6 @@ object AvroProducer extends App with Config with Logging {
   shutdownProducer()
 
   private def writeToKafka(person: Person)(implicit personToRecord: ToRecord[Person]): Unit = {
-
-    personSerializer((topic, person: Person) => kafkaAvroSerializer.serialize(topic, personToRecord.to(person)))
 
     val genericPerson: GenericRecord = new GenericData.Record(schema)
     genericPerson.put("name", person.name)
@@ -53,9 +43,6 @@ object AvroProducer extends App with Config with Logging {
     }
   }
 
-  private def personSerializer[Person](f: (String, Person) => Array[Byte]): KafkaSerializer[Person] =
-    (topic: String, data: Person) => f(topic, data)
-
   private def setupKafkaProps: Properties = {
     val props: Properties = new Properties()
     props.put("bootstrap.servers", appConfig.kafka.brokerurl)
@@ -63,18 +50,6 @@ object AvroProducer extends App with Config with Logging {
     props.put("value.serializer", "io.confluent.kafka.serializers.KafkaAvroSerializer")
     props.put("schema.registry.url", appConfig.kafka.schemaregurl)
     props
-  }
-
-  private def configureAvroSerializer: KafkaAvroSerializer = {
-    val schemaRegistryClient: SchemaRegistryClient =
-      new CachedSchemaRegistryClient(appConfig.kafka.schemaregurl, 100)
-
-    val serializerConf: util.Map[String, String] =
-      Map(VALUE_SUBJECT_NAME_STRATEGY -> classOf[TopicNameStrategy].getName, SCHEMA_REGISTRY_URL_CONFIG -> "").asJava
-
-    val kafkaAvroSerializer: KafkaAvroSerializer = new KafkaAvroSerializer(schemaRegistryClient)
-    kafkaAvroSerializer.configure(serializerConf, false)
-    kafkaAvroSerializer
   }
 
   private def shutdownProducer(): Unit = {
